bb998742b75faf8c33466a436e1b86654701d8ca
Kane Du <kanedu2000@gmail.com>
2023-11-12 00:23:41 -0500

    add react tests

---
+++ pond-web/src/util/exp.test.ts
@@ -0,0 +1,12 @@
+import { expToLevel, levelToExp } from "./exp";
+
+describe('test exp functions', () => {
+    it('exp to level are equal', () => {
+        const expectedExp = 1047;
+        const expectedLevel = 22;
+        const exp = levelToExp(expectedLevel);
+        const level = expToLevel(expectedExp);
+        expect(exp).toEqual(expectedExp);
+        expect(level).toEqual(expectedLevel);
+    });  
+});
