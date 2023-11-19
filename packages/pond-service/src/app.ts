cd37ed15e8aaf401a624f0b8f1b0c95a417e9783
Kane Du <kanedu2000@gmail.com>
2023-11-19 17:45:14 -0500

    Revert "Update app.ts"
    This reverts commit 387da0ed22b7340144bbdea46325f6d151c42623.

--- packages/pond-service/src/app.ts
+++ packages/pond-service/src/app.ts
@@ -30,6 +30,13 @@ app.use(
   })
 );
 
+// CORS Headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', POND_WEB_URL);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 // -------DB Initialization-------
 const db = knex({
   client: 'pg',
