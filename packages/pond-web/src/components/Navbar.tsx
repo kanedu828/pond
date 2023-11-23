5ec3fe96fc2776778d0624127b27ae14a67d8db1
Kane Du <kanedu2000@gmail.com>
2023-11-22 23:22:33 -0500

    Add guide

--- packages/pond-web/src/components/Navbar.tsx
+++ packages/pond-web/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Button, Container, Flex } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useLogout } from "../hooks/UseAuthClient";
 import { Collection } from "./Collection";
+import { GuideModal } from "./GuideModal";
 import { Leaderboard } from "./Leaderboard";
 
 export const Navbar = () => {
@@ -10,6 +11,7 @@ export const Navbar = () => {
 
     const [isCollectionOpen, { open: openCollection, close: closeCollection }] = useDisclosure(false);
     const [isLeaderboardOpen, { open: openLeaderboard, close: closeLeaderboard }] = useDisclosure(false);
+    const [isGuideOpen, { open: openGuide, close: closeGuide }] = useDisclosure(false);
 
     const onClickLogout = async () => {
         await logout();
@@ -19,10 +21,12 @@ export const Navbar = () => {
         <>
             <Leaderboard isOpen={isLeaderboardOpen} close={closeLeaderboard}/>
             <Collection isOpen={isCollectionOpen} close={closeCollection}/>
+            <GuideModal isOpen={isGuideOpen} close={closeGuide}/>
             <Container p={15}>
                 <Flex gap='xl' justify='center'>
                     <Button variant="subtle" color='pondTeal' size='xl' radius='md' onClick={openCollection}>Collection</Button>
                     <Button variant="subtle" color='pondTeal' size='xl' radius='md' onClick={openLeaderboard}>Leaderboard</Button>
+                    <Button variant="subtle" color='pondTeal' size='xl' radius='md' onClick={openGuide}>Guide</Button>
                     <Button variant="subtle" color='pondTeal' size='xl' radius='md' onClick={onClickLogout}>
                         Logout
                     </Button>
